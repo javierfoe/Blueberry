5689986c29e4a19d1b5f37eb60320d5c70f5f6e2
javierfoe <javier.foe@outlook.es>
2022-06-18 21:00:40 +0200

    Simplify Mirror scenes

---
+++ Assets/Blueberry/Scripts/AndroidDemo.cs
@@ -0,0 +1,38 @@
+using System;
+using Mirror;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace javierfoe.Blueberry.Hub
+{
+    public class AndroidDemo : MonoBehaviour
+    {
+        [Serializable]
+        public class ButtonScene
+        {
+            public Button button;
+            [Scene]
+            public string scene;
+        }
+        
+        [SerializeField]
+        private ButtonScene[] buttonScenes = null;
+
+        private void Awake()
+        {
+            Button button;
+            for (int i = 0; i < buttonScenes.Length; i++)
+            {
+                string scene = buttonScenes[i].scene;
+                button = buttonScenes[i].button;
+                button.onClick.AddListener(() => LoadScene(scene));
+            }
+        }
+
+        private void LoadScene(string scene)
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
